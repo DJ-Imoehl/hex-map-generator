7d6c1b7fea3cd6ae2cc515371603513fe4c11403
DJ Imoehl <dj.imoehl@gmail.com>
2021-05-31 13:20:07 -0500

    Using polygons instead of three separate divs to create hexagons

--- src/Hexagon.js
+++ src/Hexagon.js
@@ -1,23 +1,79 @@
 import React, {Component} from 'react';
 
 class Hexagon extends React.Component{
+  getHexagonPoints(size){
+    let points = [];
+    //Points are in [x, y] format
+    //point A
+    points[0] = [size, 0];
+    // point B
+    points[1] = [size/2, Math.sqrt(3)*size/2];
+    //point C
+    points[2] = [(-1)*size/2, Math.sqrt(3)*size/2];
+    //point D
+    points[3] = [(-1)*size, 0];
+    //point E
+    points[4] = [(-1)*size/2, (-1)*Math.sqrt(3)*size/2];
+    //point F
+    points[5] = [(1)*size/2, (-1)*Math.sqrt(3)*size/2];
+    return points;
+  }
+
+  rotate(points, degrees){
+    const radians = degrees*(Math.PI/180);
+    for(var i = 0; i < points.length; i++){
+      const oldPoint = points[i];
+      const x = oldPoint[0];
+      const y = oldPoint[1];
+
+      //Using matrix transform
+      const newX = x*Math.cos(radians) - y*Math.sin(radians);
+      const newY = x*Math.sin(radians) + y*Math.cos(radians);
+
+      points[i] = [newX, newY];
+    }
+  }
+
+  translateX(points, unitsMoved){
+    points.forEach(point => point[0] = point[0] + unitsMoved);
+  }
+
+  translateY(points, unitsMoved){
+    points.forEach(point => point[1] = point[1] + unitsMoved);
+  }
+
     render() {
       const {color} = this.props
-      const topStyle = {
-        borderBottomColor: color
+      const {size} = this.props
+      const {columns} = this.props
+      const {columnPosition} = this.props
+      const {rowPosition} = this.props
+      const {isEvenRow} = this.props
+
+      let points = this.getHexagonPoints(size);
+      this.rotate(points, 90);
+      this.translateX(points, 3*size*columnPosition/Math.sqrt(3));
+      this.translateY(points, size*rowPosition*(3/2));
+      if(isEvenRow == "Y"){
+        const evenRowTranslation = 3*size/(2*Math.sqrt(3));
+        this.translateX(points, evenRowTranslation);
       }
-      const middleStyle = {
-        backgroundColor: color
-      }
-      const bottomStyle = {
-        borderTopColor: color
-      }
+      let stringPoints = "";
+      
+      points.forEach(point => stringPoints += point[0].toFixed(8) + "," + point[1].toFixed(8) + " ");
+      console.log(stringPoints);
+
+      
+      // return (
+      //   <div className="hex">
+      //     <div className="top" style={topStyle}></div>
+      //     <div className="middle" style={middleStyle}></div>
+      //     <div className="bottom" style={bottomStyle}></div>
+      //   </div>
+      // );
+
       return (
-        <div className="hex">
-          <div className="top" style={topStyle}></div>
-          <div className="middle" style={middleStyle}></div>
-          <div className="bottom" style={bottomStyle}></div>
-        </div>
+          <polygon className="hexagon" points={stringPoints} fill={color}/>
       );
     }
 }
--- src/HexagonMap.js
+++ src/HexagonMap.js
@@ -1,23 +1,77 @@
 import React, {Component} from 'react';
-import HexagonRow from './HexagonRow.js';
+import Hexagon from './Hexagon.js';
+
+let hexType = {
+    PLAIN: "PLAIN",
+    HILL: "HILL",
+    FOREST: "FOREST",
+    DESERT: "DESERT",
+    MOUNTAIN: "MOUNTAIN"
+};
+const hexTypeSize = 5;
 
 class HexagonMap extends React.Component{
+
+    generateHexType(){
+        let numHexTypes = hexType.size;
+        let randomNumber = Math.floor(Math.random()*hexTypeSize);
+        return Object.keys(hexType)[randomNumber];
+    }
+
+    getHexColor(){
+        let thisHexType = this.generateHexType();
+        switch(thisHexType){
+        case hexType.PLAIN:
+            return "Green";
+        case hexType.HILL:
+            return "Yellow";
+        case hexType.FOREST:
+            return "Red";
+        case hexType.DESERT:
+            return "Brown";
+        case hexType.MOUNTAIN:
+            return "Gray";
+
+        }
+    }
+
+    makeRow(hexagonSize, columns, rowPosition){
+        var hexagons = [];
+        for(var i = 0; i < columns; i++){
+            var color = this.getHexColor();
+            var isEvenRow = (rowPosition + 1) % 2 == 0 ? "Y" : "N";
+            hexagons.push(<Hexagon color={color} size={hexagonSize} columns={columns} columnPosition={i} rowPosition={rowPosition} isEvenRow={isEvenRow}/>);
+        }
+
+        return hexagons;
+    }
+
     render(){
         const columns = 5;
         const rows = 5;
+        const hexagonSize = 2;
 
         var items = [];
 
         for(var i = 0; i < rows; i++){
-        var hexRowClass = (i + 1) % 2 == 0 ? "hex-row even" : "hex-row";
-        items.push(<div className={hexRowClass}>
-                    <HexagonRow columns={columns}/>
-                    </div>);
+            var isEvenRow = (i + 1) % 2 == 0 ? "Y" : "N";
+            let hexagons = this.makeRow(hexagonSize, columns, i);
+            items.push(hexagons);
         }
+
+        let percentWidth = 60;//want max of 60% width on our viewbox elements
+        percentWidth = percentWidth/columns;
+        console.log(percentWidth);
+        const stringPercentWidth = "" + percentWidth + "%";
+        console.log(stringPercentWidth);
+
+        let viewBoxValue = "" + (-1)*hexagonSize + " " + (-1)*hexagonSize + " " + 10*columns + " " + 2*hexagonSize*rows;
 
         return (
         <div>
-            {items}
+            <svg viewBox={viewBoxValue} style={{width: {stringPercentWidth}}}>
+                {items}
+            </svg>
         </div>
         )
     }
